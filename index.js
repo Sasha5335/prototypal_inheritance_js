d00912235bb0346bf28ebe62ce846f4873a314e8
Aleksandr <aleksandr@gmail.com>
2021-01-16 14:42:36 +0200

    Function => Class

--- index.js
+++ index.js
@@ -1,136 +1,135 @@
 'use strict';
 
 function MyArrayProto() {
-	this.push = function push() {
-		for (let i = 0; i < arguments.length; i++) {
-			this[this.length++] = arguments[i];
-		}
-		return this.length;
-	};
+   this.push = function push() {
+      for (let i = 0; i < arguments.length; i++) {
+         this[this.length++] = arguments[i];
+      }
+      return this.length;
+   };
+
+   this.pop = function () {
+      if (this.length === 0) {
+         return;
+      }
+      const lastItem = this[this.length - 1];
+      delete this[--this.length];
+      return lastItem;
+   };
 
 
-	this.pop = function () {
-		if (this.length === 0) {
-			return;
-		}
-		const lastItem = this[this.length - 1];
-		delete this[--this.length];
-		return lastItem;
-	};
+   this.unshift = function unsift() {
+      const arrArguments = [];
+      for (let i = 0; i < arguments.length; i++) {
+         arrArguments.push(arguments[i]);
+      }
+      for (let i = 0; i < this.length; i++) {
+         arrArguments.push(this[i]);
+      }
+      for (let i = 0; i < arrArguments.length; i++) {
+         this[i] = arrArguments[i];
+      }
+      return this.length = arrArguments.length;
+   };
 
 
-	this.unshift = function unsift() {
-		const arrArguments = [];
-		for (let i = 0; i < arguments.length; i++) {
-			arrArguments.push(arguments[i]);
-		}
-		for (let i = 0; i < this.length; i++) {
-			arrArguments.push(this[i]);
-		}
-		for (let i = 0; i < arrArguments.length; i++) {
-			this[i] = arrArguments[i];
-		}
-		return this.length = arrArguments.length;
-	};
+   this.shift = function shift() {
+      const arrArguments = [];
+      const firstItem = this[0];
+
+      if (this.length === 0) {
+         return;
+      }
+      for (let i = 1; i < this.length; i++) {
+         arrArguments.push(this[i]);
+      }
+      for (let i = 0; i < arrArguments.length; i++) {
+         this[i] = arrArguments[i];
+      }
+      delete this[--this.length];
+      return firstItem;
+   };
 
 
-	this.shift = function shift() {
-		const arrArguments = [];
-		const firstItem = this[0];
-
-		if (this.length === 0) {
-			return;
-		}
-		for (let i = 1; i < this.length; i++) {
-			arrArguments.push(this[i]);
-		}
-		for (let i = 0; i < arrArguments.length; i++) {
-			this[i] = arrArguments[i];
-		}
-		delete this[--this.length];
-		return firstItem;
-	};
+   this.concat = function concat() {
+      const arrConcat = new MyArray();
+      for (let i = 0; i < this.length; i++) {
+         arrConcat.push(this[i]);
+      }
+      for (let i = 0; i < arguments.length; i++) {
+         arrConcat.push(arguments[i]);
+      }
+      return arrConcat;
+   };
 
 
-	this.concat = function concat() {
-		const arrConcat = new MyArray();
-		for (let i = 0; i < this.length; i++) {
-			arrConcat.push(this[i]);
-		}
-		for (let i = 0; i < arguments.length; i++) {
-			arrConcat.push(arguments[i]);
-		}
-		return arrConcat;
-	};
+   this.revers = function () {
+      const arrRevers = [];
+      for (let i = this.length - 1; i >= 0; i--) {
+         arrRevers.push(this[i]);
+      }
+      for (let i = 0; i < arrRevers.length; i++) {
+         this[i] = arrRevers[i];
+      }
+      return this;
+   };
 
 
-	this.revers = function () {
-		const arrRevers = [];
-		for (let i = this.length - 1; i >= 0; i--) {
-			arrRevers.push(this[i]);
-		}
-		for (let i = 0; i < arrRevers.length; i++) {
-			this[i] = arrRevers[i];
-		}
-		return this;
-	};
+   this.forEach = function forEach(func) {
+      for (let i = 0; i < this.length; i++) {
+         func(this[i]);
+      }
+   };
 
 
-	this.forEach = function forEach(func) {
-		for (let i = 0; i < this.length; i++) {
-			func(this[i]);
-		}
-	};
+   this.map = function map(func) {
+      const arrMap = new MyArray();
+      for (let i = 0; i < this.length; i++) {
+         arrMap.push(func(this[i], i, this));
+      }
+      return arrMap;
+   };
 
 
-	this.map = function map(func) {
-		const arrMap = new MyArray();
-		for (let i = 0; i < this.length; i++) {
-			arrMap.push(func(this[i], i, this));
-		}
-		return arrMap;
-	};
+   this.some = function some(func) {
+      for (let i = 0; i < this.length; i++) {
+         if (func(this[i], i, this)) {
+            return true;
+         }
+      }
+      return false;
+   };
 
 
-	this.some = function some(func) {
-		for (let i = 0; i < this.length; i++) {
-			if (func(this[i], i, this)) {
-				return true;
-			}
-		}
-		return false;
-	};
+   this.every = function every(func) {
+      for (let i = 0; i < this.length; i++) {
+         if (!func(this[i], i, this)) {
+            return false;
+         }
+      }
+      return true;
+   };
 
 
-	this.every = function every(func) {
-		for (let i = 0; i < this.length; i++) {
-			if (!func(this[i], i, this)) {
-				return false;
-			}
-		}
-		return true;
-	};
-
-
-	this.filter = function filter(checkFunction) {
-		const result = new MyArray();
-		for (let i = 0; i < this.length; i++) {
-			if (checkFunction(this[i], i, this)) {
-				result.push(this[i]);
-			}
-		}
-		return result;
-	};
+   this.filter = function filter(checkFunction) {
+      const result = new MyArray();
+      for (let i = 0; i < this.length; i++) {
+         if (checkFunction(this[i], i, this)) {
+            result.push(this[i]);
+         }
+      }
+      return result;
+   };
 }
 
 
 /* Объекты с данными */
 function MyArray(...args) {
-	this.length = 0;
+   this.length = 0;
 
-	for (let i = 0; i < arguments.length; i++) {
-		this.push(arguments[i]);
-	}
+   for (let i = 0; i < arguments.length; i++) {
+      this.push(arguments[i]);
+   }
 }
 
 
